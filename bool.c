256c40b5169416e121a5de3715948fb338da31dc
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-03-24 01:02:30 +0300

    C based simple tasks

---
+++ bool.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h> // import boolean header
+
+int main ()
+{
+	bool codingIsfun = true;//1
+	bool educationPays = false;//0
+
+	printf("%d\n", codingIsfun);
+	printf("%d\n", educationPays);
+
+	int a = 9;
+	int b = 6;
+
+	printf("%d\n", b < a);//prints 1 if true 0 if false 
+
+	return 0;
+}
