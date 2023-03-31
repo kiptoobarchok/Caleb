46b94931454c6b97a4565dc0213f0ccfe0906af3
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-03-31 03:09:45 +0300

    start of C++

--- C++/first_program.cpp
+++ C++/first_program.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello Caleb\n";
+	cout << "Hello Caleb\n";//prints out a string
 	cout << "Welcome to C++\n";
 
-	int x = 5;
+	int x = 5;//initializing a variable
 
-	cin >> x;
+	cin >> x;//asks the user to key in input 
 
-	cout <<"x : " << x;
+	cout <<"x : " << x;//>>(input) <<(output)
 
 	return 0;
 }
