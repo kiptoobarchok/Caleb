405d8c7f173af5f5a7366cd047f7f18dfdf1bef9
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-04-04 02:08:54 +0300

    Cpp variables

--- C++/variables.cpp
+++ C++/variables.cpp
@@ -7,6 +7,16 @@ int main()
 
 	age = 21; // assignment
 
+	int age_1,age_2,age_3;
+
+	age_3 = 34;
+
+	cin >> age_1 >> age_2 ;
+
+	cout << "age_3 :" <<age_3 << endl;
+
+	cout << "age_1 :" << age_1 << endl<< "age_2 : " << age_2 << endl;
+
 	cout << "age :" << age << endl;
 
 	return 0;
