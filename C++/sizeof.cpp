e8157dca90ba1b0a9036b6d3170b5fce39e6b268
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-04-08 11:27:48 +0300

    size of datatypes

--- C++/sizeof.cpp
+++ C++/sizeof.cpp
@@ -9,10 +9,10 @@ int main()
 	float height = 23.34;
 	double height_2 = 23.34546267;
 
-	cout << sizeof(age)<< "- size of int" <<endl;
-	cout << sizeof(grade)<< "- size of char" <<endl;
-	cout << sizeof(bool)<< "- size of boolean" <<endl;
-	cout << sizeof(float)<< "- size of float" <<endl;
-	cout << sizeof(double)<< "- size of double" <<endl; // for more precision hence it holds more space
+	cout << sizeof(age)<< "bytes- size of int" <<endl;
+	cout << sizeof(grade)<< "bytes- size of char" <<endl;
+	cout << sizeof(bool)<< "bytes- size of boolean" <<endl;
+	cout << sizeof(float)<< "bytes- size of float" <<endl;
+	cout << sizeof(double)<< "bytes- size of double" <<endl; // for more precision hence it holds more space
 }
 	
