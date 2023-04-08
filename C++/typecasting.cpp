6382bba5f9484f1901f75c6425c3f6cc002d9b4f
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-04-08 12:23:04 +0300

    Implicit and explicit typecasting

---
+++ C++/typecasting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+//typecasting is when dealing with more than one type of data 
+
+int main()
+{
+
+	cout << 5/3 << " : resulting data type" <<endl; // implicit typecasting (compiler automatically changes datatype)
+	cout << (float)5/3 << " : resulting data type" << endl;//explicit typecasting
+
+}
