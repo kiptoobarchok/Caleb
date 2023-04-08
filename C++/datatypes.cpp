7747ae406384a38cb26476d5c6c7af2802ec4d5d
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-04-08 11:00:25 +0300

    Cpp datatypes

---
+++ C++/datatypes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+
+int main()
+{
+	int age = 21;
+	char grade = 'B';
+	bool is_true = true;
+	float height = 23.34;
+	double height_2 = 23.34546267;
+
+	cout << age << endl;
+	cout << grade << endl;
+	cout << is_true << endl;
+	cout << fixed << setprecision (8) << height << endl;
+	cout << fixed << setprecision (8) << height_2<<endl;
+
+
+}
