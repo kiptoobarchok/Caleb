b3d4f8903f48ec44e129922af448e2ffd19a9f6f
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-04-10 15:58:56 +0300

    use of else if for multiple conditions

--- C++/grading.cpp
+++ C++/grading.cpp
@@ -7,7 +7,7 @@ using namespace std;
  *60 - 69 : F
  *50 - 59 : D
  *40 - 49 : E
- * <40 - F
+ *   < 40 - F
  */
 
 int main()
@@ -20,33 +20,35 @@ int main()
 
 	cout << "marks : " << marks << endl;
 
-	if (marks >= 90 && marks == 100 )
+	if (marks >= 90 && marks <= 100 )
 	{
 		cout << "A" <<endl;
 	}
-	else if(marks >= 70)
+	else if(marks >= 70 && marks <= 79)
 	{
 		cout << "B" << endl;
 	}
-    else if(marks >= 70)
- 	{
-		cout << "B" << endl;
-	}
-	else if(marks >= 60)
+
+	else if(marks >= 60 && marks <= 69)
  	{
 		cout << "C" << endl;
 	}
-	else if(marks >= 50)
+	else if(marks >= 50 && marks <= 59)
  	{
 		cout << "D" << endl;
 	}
-	else if(marks >= 40)
+	else if(marks >= 40 && marks <= 49)
  	{
 		cout << "E" << endl;
 	}
-	else
+	else if(  marks >= 0 && marks <= 39)
  	{
 		cout << "F" << endl;
+	}
+
+	else 
+	{
+		cout << "The school marks ranges from 0 - 100." <<endl <<"Please enter valid marks" << endl;
 	}
 
 	cout << "This grading system is according to the schools dean of academics" << endl;
