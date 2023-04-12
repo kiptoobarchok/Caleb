c2815b5727c34a1d14708de02eab58016651ab6f
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-04-12 21:15:36 +0300

    string concantation

---
+++ C++/two_strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string name = "Caleb ";
+	string second_name = "Kiptoo";
+	string fullname = name + second_name;
+
+	cout << fullname << endl;
+}
