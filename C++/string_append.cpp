eab7e2cc6a9022a3b3934a438f189c1009add47f
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-04-18 20:31:07 +0300

    using append to + two strings

---
+++ C++/string_append.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string name = "Caleb ";
+	string sec_name = "Kiptoo";
+
+	string fullname = name.append(sec_name);
+
+	cout << fullname << endl;
+}
