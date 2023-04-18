8186e3d4c4aff5f5dd68ebb61c28ebb689818382
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-04-18 22:28:45 +0300

    special characters in cpp

--- C++/special_char.cpp
+++ C++/special_char.cpp
@@ -3,8 +3,17 @@
 
  int main()
  {
+ 	string name;
+
  	cout << "I am Caleb \"Boyo\" Kiptoo" << endl;
 
  	cout << "It\'s totally ok to call me that "<< endl;
+
+ 	cout << "Can u enter your full names : " << endl;
+ 	cin >> name;
+ 	getline(cin,name);
+
+ 	cout << "Your name is : " << endl;
+ 	
  	return 0;
  }
