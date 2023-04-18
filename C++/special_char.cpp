1de0177ab8549b93c1b56f20558e65cae8ba38d0
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-04-18 22:13:35 +0300

    special characters in cpp

---
+++ C++/special_char.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+ using namespace std;
+
+ int main()
+ {
+ 	cout << "I am Caleb \"Boyo\" Kiptoo" << endl;
+
+ 	cout << "It\'s totally ok to call me that "<< endl;
+ 	return 0;
+ }
