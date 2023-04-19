b36892594cf3af6589cfd29f2eec2f1df9231643
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-04-19 21:55:42 +0300

    Switch case in cpp

---
+++ C++/math.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int x,y;
+	x = 21;
+	y = 2;
+
+	cout << max(x,y) << endl;
+
+	cout << sqrt(64) << endl;
+
+	cout << log(2) << endl;
+}
---
+++ C++/switch.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/* code */
+
+	int day;
+
+	cout << "Enter the day of the week : " << endl;
+
+	cin >> day;
+
+	cout << "It is " ;
+
+	switch(day)
+	{
+	case 1:
+		cout << "Monday"<<endl;
+		break;
+
+	case 2:
+		cout << "Tuesday"<<endl;
+		break;
+
+	case 3:
+		cout << "Wednesday"<<endl;
+		break;
+
+	case 4:
+		cout << "Thursday"<<endl;
+		break;
+
+	case 5:
+		cout << "Friday"<<endl;
+		break;
+
+	case 6:
+		cout << "Saturday"<<endl;
+		break;
+
+	case 7:
+		cout << "Sunday"<<endl;
+		break;
+
+	default:
+		cout << "invalid"<<endl;
+		break;
+
+	}
+
+	return 0;
+}
